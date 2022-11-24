09f9f6f137e535e030e7807af37020c12ccf0b7b
TheKelp1e <vova_krasnov_2004@mail.ru>
2022-11-24 19:09:35 +0300

    Initial commit

---
+++ src/main/java/com/example/elobit/controllers/MainController.java
@@ -0,0 +1,2 @@
+package com.example.elobit.controllers;public class MainController {
+}
---
+++ src/test/java/com/example/elobit/ELoBitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elobit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELoBitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
