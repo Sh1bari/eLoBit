9aa745437ba61ea5811f287d5e81ed0daf604ed2
TheKelp1e <vova_krasnov_2004@mail.ru>
2022-11-24 20:18:24 +0300

    1 commit

--- src/main/java/com/example/elobit/controllers/rest/TestController.java
+++ src/main/java/com/example/elobit/controllers/rest/TestController.java
@@ -3,6 +3,7 @@ package com.example.elobit.controllers.rest;
 import com.example.elobit.models.UserAuthorisation;
 import com.example.elobit.repo.UserAuthorisationRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +14,7 @@ public class TestController {
     @Autowired
     private UserAuthorisationRepo userAuthorisationRepo;
 
+    @CrossOrigin
     @GetMapping("/")
     private List<UserAuthorisation> home(){
         List<UserAuthorisation> user = (List<UserAuthorisation>) userAuthorisationRepo.findAll();
